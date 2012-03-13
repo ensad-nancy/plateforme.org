8c5fa0e4b28bc2662fa8e7f407928f257192e26b
dcfvg <qwsx@dcfvg.com>
2012-03-13 16:46:16 +0100

    Début de la version 2

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE >
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title></title>
+    <meta charset="utf-8">
+    <title>Plateforme — ensa Nancy</title>
     <link rel="stylesheet" type="text/css" href="_lib/baseline.reset.css" />
     <link rel="stylesheet" type="text/css" href="_lib/platforme.css" />
+    <script src="_lib/js/modernizr.custom.26096.js" type="text/javascript"></script>
 </head>
 
-<style>
-body { background: #BEBEBE repeat-y 30%;}
-div { margin-left:30%; padding-left:60px;margin-top: 20%; color: white;}
-a {color: white;}
-a:hover { text-decoration: underline;}
-p {text-align: right; margin-right: 10%;}
-
-
-</style>
 <body>
-<div>
-<h1>plateforme</h1>
-<p><a href="mod/sandbox">sandbox > </a></p>
-<p><a href="mod/capture">capture > </a></p>
-<p><a href="mod/profil">profils > </a></p>
-</div>
+<nav>
+	<ul>
+		<li><a href="/">plateforme</a></li>
+		<li><a href="">erasmus</a></li>
+		<li><a href="">evenements</a></li>
+	</ul>
+</nav>
+
+<section class="gImage">
+	<img src="https://docs.google.com/drawings/pub?id=1wuZ_TwEpJIXGK12ZhMyLmLXmZtjktvbXP3m3mUh1zxE&amp;w=2502&amp;h=2355">
+</section>
+
 </body>
