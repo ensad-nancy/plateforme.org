dcf987bd10345ad5c6847801a05ccc997f852b8f
dcfvg <qwsx@dcfvg.com>
2012-03-19 10:54:51 +0100

    Ajout du menu et des rubriques

--- index.php
+++ index.php
@@ -7,19 +7,46 @@
     <link rel="stylesheet" type="text/css" href="_lib/baseline.reset.css" />
     <link rel="stylesheet" type="text/css" href="_lib/platforme.css" />
     <script src="_lib/js/modernizr.custom.26096.js" type="text/javascript"></script>
+    
+    <script src="_lib/js/jquery.min.js"></script>
+	<script src="_lib/js/jquery-ui.min.js"></script>
+	
+	<script>
+  $(document).ready(function() {
+    $("#draggable").draggable();
+  });
+  </script>
 </head>
 
 <body>
-<nav>
-	<ul>
+<nav id="draggable">
+	<h1>Multimodale graphique/ENSAN</h1>&#8212;
+	<ol>
 		<li><a href="/">plateforme</a></li>
-		<li><a href="">erasmus</a></li>
-		<li><a href="">evenements</a></li>
-	</ul>
+		<li><a href="?p=gratuit">Objet gratuit</a></li>
+		<li><a href="?p=nouvelleimage">nouvelle image</a></li>
+		<li><a href="?p=erasmus2012">échanges 2012</a></li>
+		<li>manifeste </li>
+	</ol>
 </nav>
 
-<section class="gImage">
-	<img src="https://docs.google.com/drawings/pub?id=1wuZ_TwEpJIXGK12ZhMyLmLXmZtjktvbXP3m3mUh1zxE&amp;w=2502&amp;h=2355">
-</section>
+<?
+
+	switch ($_GET['p']) {
+    case "gratuit":
+        include('pages/006.gratuit.php');
+        break;
+    case "erasmus2012":
+        include('pages/005.echanges2012.php');
+        
+        break;
+    case "nouvelleimage":
+        echo '<iframe class="ext" src="pages/004.nouvelle-image.php"></iframe>';
+        break;
+    default:
+    	include('pages/000.home.php');
+	}
+
+?>
 
 </body>
---
+++ pages/000.home.php
@@ -0,0 +1,3 @@
+<section class="gImage">
+	<img src="https://docs.google.com/drawings/pub?id=1wuZ_TwEpJIXGK12ZhMyLmLXmZtjktvbXP3m3mUh1zxE&amp;w=2502&amp;h=2355">
+</section>
